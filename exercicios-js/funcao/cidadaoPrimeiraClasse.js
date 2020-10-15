6e776839b72d8b21eea9dd3a2a60082b21e766b6
KAPPO <marcelo.capobianco@outlook.com>
2020-10-15 19:25:38 -0300

    Capitulo sobre Funções

---
+++ exercicios-js/funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,9 @@
+// Função em JS é First-Class Object (Citzens)
+// Higher-order function
+
+/* 
+Isto se dá quando a linguagem trata a função como dado, ou seja é possivel usa-la como um parametro
+Quando se existe uma outra função dentro da função, diferente de quando antigamente isto era tratado 
+de forma bem separada. Assim abre-se um leque de possibilidades.
+*/
+
